6e0694358c91d17a10c0df930891ae38c4f8b5c2
xovest <79008917+xovest@users.noreply.github.com>
2021-12-02 22:57:18 +0200

    build wrks

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 
-const app = express();
+const app: Application = express();
 
-app.get('/', (req, res) => {
+const add = (n: number, m: number): number => n + m;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log(add(2, 6));
   res.send('Yo');
 });
 
